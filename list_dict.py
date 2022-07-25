ceab25cbc8e8f8c092d58d993188e64d197a1b5a
Joy Patel <joypatel24102001@gmail.com>
2022-07-25 17:49:27 +0530

    All files added

---
+++ list_dict.py
@@ -0,0 +1,599 @@
+dict = {
+    "success": True,
+    "data": [
+        {
+            "id": 1033,
+            "clinical_rotation_type": "Behavioral Medicine"
+        },
+        {
+            "id": 1034,
+            "clinical_rotation_type": "Sleep Medicine"
+        },
+        {
+            "id": 1035,
+            "clinical_rotation_type": "Addiction Medicine"
+        },
+        {
+            "id": 1036,
+            "clinical_rotation_type": "Child & Adolescent Psychiatry"
+        },
+        {
+            "id": 1037,
+            "clinical_rotation_type": "Clinical Neurophysiology"
+        },
+        {
+            "id": 1038,
+            "clinical_rotation_type": "Consultation-Liaison Psychiatry"
+        },
+        {
+            "id": 1039,
+            "clinical_rotation_type": "Forensic Psychiatry"
+        },
+        {
+            "id": 1040,
+            "clinical_rotation_type": "General Adult Psychiatry"
+        },
+        {
+            "id": 1041,
+            "clinical_rotation_type": "Geriatric Psychiatry"
+        },
+        {
+            "id": 1042,
+            "clinical_rotation_type": "Lesbian, Gay, Bisexual and Transgender Mental Health and Emotional Support Services"
+        },
+        {
+            "id": 1043,
+            "clinical_rotation_type": "Military Psychiatry"
+        },
+        {
+            "id": 1044,
+            "clinical_rotation_type": "Pain Medicine"
+        },
+        {
+            "id": 1045,
+            "clinical_rotation_type": "Psychiatric Emergency Medicine"
+        },
+        {
+            "id": 1046,
+            "clinical_rotation_type": "Psychosomatic Medicine"
+        },
+        {
+            "id": 1047,
+            "clinical_rotation_type": "Cardiology"
+        },
+        {
+            "id": 1048,
+            "clinical_rotation_type": "General Clinical Cardiology"
+        },
+        {
+            "id": 1049,
+            "clinical_rotation_type": "Cardiothoracic Surgery"
+        },
+        {
+            "id": 1050,
+            "clinical_rotation_type": "Thoracic Surgery"
+        },
+        {
+            "id": 1051,
+            "clinical_rotation_type": "Vascular Surgery"
+        },
+        {
+            "id": 1052,
+            "clinical_rotation_type": "Echocardiology"
+        },
+        {
+            "id": 1053,
+            "clinical_rotation_type": "Electrophysiology"
+        },
+        {
+            "id": 1054,
+            "clinical_rotation_type": "Interventional Cardiology"
+        },
+        {
+            "id": 1055,
+            "clinical_rotation_type": "Adult Congenital Cardiology"
+        },
+        {
+            "id": 1056,
+            "clinical_rotation_type": "Nuclear Cardiology"
+        },
+        {
+            "id": 1057,
+            "clinical_rotation_type": "Heart Failure and Transplant Cardiology"
+        },
+        {
+            "id": 1058,
+            "clinical_rotation_type": "Computed Tomography/Magnetic Resonance Imaging Cardiology"
+        },
+        {
+            "id": 1059,
+            "clinical_rotation_type": "Pediatric Cardiology"
+        },
+        {
+            "id": 1060,
+            "clinical_rotation_type": "Clinical Research"
+        },
+        {
+            "id": 1061,
+            "clinical_rotation_type": "Critical Care"
+        },
+        {
+            "id": 1062,
+            "clinical_rotation_type": "ICU - Intensive Care Unit"
+        },
+        {
+            "id": 1063,
+            "clinical_rotation_type": "SICU - Surgical Intensive Care Unit"
+        },
+        {
+            "id": 1064,
+            "clinical_rotation_type": "MICU - Medical Intensive Care Unit"
+        },
+        {
+            "id": 1065,
+            "clinical_rotation_type": "CCU - Coronary Care Unit"
+        },
+        {
+            "id": 1066,
+            "clinical_rotation_type": "CTICU - Cardiothoracic Intensive Care Unit"
+        },
+        {
+            "id": 1067,
+            "clinical_rotation_type": "NICU - Newborn Intensive Care Unit"
+        },
+        {
+            "id": 1068,
+            "clinical_rotation_type": "NICU - Neurosurgical Intensive Care Unit"
+        },
+        {
+            "id": 1069,
+            "clinical_rotation_type": "PICU - Pediatric Intensive Care Unit"
+        },
+        {
+            "id": 1070,
+            "clinical_rotation_type": "Burn ICU"
+        },
+        {
+            "id": 1072,
+            "clinical_rotation_type": "Dermatology"
+        },
+        {
+            "id": 1073,
+            "clinical_rotation_type": "Dermato-oncology"
+        },
+        {
+            "id": 1074,
+            "clinical_rotation_type": "Dermatopathology"
+        },
+        {
+            "id": 1075,
+            "clinical_rotation_type": "Immunodermatology"
+        },
+        {
+            "id": 1076,
+            "clinical_rotation_type": "Pediatric Dermatology"
+        },
+        {
+            "id": 1078,
+            "clinical_rotation_type": "Cosmetic Dermatology"
+        },
+        {
+            "id": 1079,
+            "clinical_rotation_type": "Emergency Medicine"
+        },
+        {
+            "id": 1080,
+            "clinical_rotation_type": "Endocrinology"
+        },
+        {
+            "id": 1081,
+            "clinical_rotation_type": "Diabetes & Metabolism"
+        },
+        {
+            "id": 1082,
+            "clinical_rotation_type": "Endocrine Oncology"
+        },
+        {
+            "id": 1083,
+            "clinical_rotation_type": "Endocrinology Nuclear Medicine"
+        },
+        {
+            "id": 1084,
+            "clinical_rotation_type": "Gender Reassignment"
+        },
+        {
+            "id": 1085,
+            "clinical_rotation_type": "Neuroendocrinology"
+        },
+        {
+            "id": 1086,
+            "clinical_rotation_type": "Pediatric Endocrinology"
+        },
+        {
+            "id": 1087,
+            "clinical_rotation_type": "Reproductive Endocrinology"
+        },
+        {
+            "id": 1088,
+            "clinical_rotation_type": "Thyroid Disease"
+        },
+        {
+            "id": 1089,
+            "clinical_rotation_type": "Gastroenterology"
+        },
+        {
+            "id": 1091,
+            "clinical_rotation_type": "Hepatology"
+        },
+        {
+            "id": 1092,
+            "clinical_rotation_type": "Pediatric Gastroenterology"
+        },
+        {
+            "id": 1093,
+            "clinical_rotation_type": "Gastrointestinal Oncology"
+        },
+        {
+            "id": 1094,
+            "clinical_rotation_type": "Neurogastroenterology"
+        },
+        {
+            "id": 1095,
+            "clinical_rotation_type": "Transplant Hepatology"
+        },
+        {
+            "id": 1096,
+            "clinical_rotation_type": "Genetics & Genomics"
+        },
+        {
+            "id": 1097,
+            "clinical_rotation_type": "Clinical Genetics and Genomics"
+        },
+        {
+            "id": 1098,
+            "clinical_rotation_type": "Laboratory Genetics"
+        },
+        {
+            "id": 1099,
+            "clinical_rotation_type": "Molecular Genetics"
+        },
+        {
+            "id": 1100,
+            "clinical_rotation_type": "Genetic Counseling"
+        },
+        {
+            "id": 1101,
+            "clinical_rotation_type": "Cytogenetics"
+        },
+        {
+            "id": 1102,
+            "clinical_rotation_type": "Biochemical Genetics"
+        },
+        {
+            "id": 1103,
+            "clinical_rotation_type": "Hematology / Oncology"
+        },
+        {
+            "id": 1105,
+            "clinical_rotation_type": "Breast Oncology"
+        },
+        {
+            "id": 1106,
+            "clinical_rotation_type": "General Hematology Oncology"
+        },
+        {
+            "id": 1107,
+            "clinical_rotation_type": "Gynecologic Oncology"
+        },
+        {
+            "id": 1108,
+            "clinical_rotation_type": "Medical Oncology"
+        },
+        {
+            "id": 1109,
+            "clinical_rotation_type": "Neuro Oncology"
+        },
+        {
+            "id": 1110,
+            "clinical_rotation_type": "Orthopedic Oncology"
+        },
+        {
+            "id": 1111,
+            "clinical_rotation_type": "Radiation Oncology"
+        },
+        {
+            "id": 1112,
+            "clinical_rotation_type": "Surgical Oncology"
+        },
+        {
+            "id": 1113,
+            "clinical_rotation_type": "Infectious Disease"
+        },
+        {
+            "id": 1114,
+            "clinical_rotation_type": "COVID 19"
+        },
+        {
+            "id": 1115,
+            "clinical_rotation_type": "HIV / AIDS"
+        },
+        {
+            "id": 1116,
+            "clinical_rotation_type": "Hepatitis"
+        },
+        {
+            "id": 1118,
+            "clinical_rotation_type": "Pediatric Infectious Disease"
+        },
+        {
+            "id": 1119,
+            "clinical_rotation_type": "Public Health"
+        },
+        {
+            "id": 1120,
+            "clinical_rotation_type": "Transplant Infectious Disease"
+        },
+        {
+            "id": 1121,
+            "clinical_rotation_type": "Tropical Medicine"
+        },
+        {
+            "id": 1122,
+            "clinical_rotation_type": "Tuberculosis"
+        },
+        {
+            "id": 1123,
+            "clinical_rotation_type": "Integrative Medicine"
+        },
+        {
+            "id": 1124,
+            "clinical_rotation_type": "LGBTQ+ Health"
+        },
+        {
+            "id": 1125,
+            "clinical_rotation_type": "Gay and Bisexual Men's Health"
+        },
+        {
+            "id": 1126,
+            "clinical_rotation_type": "Gender Affirming Surgery"
+        },
+        {
+            "id": 1127,
+            "clinical_rotation_type": "Lesbian and Bisexual Health"
+        },
+        {
+            "id": 1128,
+            "clinical_rotation_type": "LGBT+ Youth"
+        },
+        {
+            "id": 1129,
+            "clinical_rotation_type": "Transgender Surgery & Medicine"
+        },
+        {
+            "id": 1130,
+            "clinical_rotation_type": "Nephrology"
+        },
+        {
+            "id": 1131,
+            "clinical_rotation_type": "Interventional Nephrology"
+        },
+        {
+            "id": 1132,
+            "clinical_rotation_type": "Kidney Dialysis"
+        },
+        {
+            "id": 1134,
+            "clinical_rotation_type": "Onconephrology"
+        },
+        {
+            "id": 1135,
+            "clinical_rotation_type": "Pediatric Nephrology"
+        },
+        {
+            "id": 1136,
+            "clinical_rotation_type": "Occupational Medicine"
+        },
+        {
+            "id": 1137,
+            "clinical_rotation_type": "Ophthalmology"
+        },
+        {
+            "id": 1138,
+            "clinical_rotation_type": "Orthopedics"
+        },
+        {
+            "id": 1139,
+            "clinical_rotation_type": "Foot And Ankle"
+        },
+        {
+            "id": 1140,
+            "clinical_rotation_type": "Hand Surgery"
+        },
+        {
+            "id": 1141,
+            "clinical_rotation_type": "Orthopedic Surgery"
+        },
+        {
+            "id": 1142,
+            "clinical_rotation_type": "Orthopedic Trauma Surgery"
+        },
+        {
+            "id": 1143,
+            "clinical_rotation_type": "Pediatric Orthopedics"
+        },
+        {
+            "id": 1144,
+            "clinical_rotation_type": "Spine Surgery"
+        },
+        {
+            "id": 1145,
+            "clinical_rotation_type": "Sports Medicine"
+        },
+        {
+            "id": 1146,
+            "clinical_rotation_type": "Total Joint Replacement"
+        },
+        {
+            "id": 1147,
+            "clinical_rotation_type": "Palliative Care"
+        },
+        {
+            "id": 1148,
+            "clinical_rotation_type": "Pediatrics"
+        },
+        {
+            "id": 1149,
+            "clinical_rotation_type": "Neonatology"
+        },
+        {
+            "id": 1150,
+            "clinical_rotation_type": "Pediatric Allergy / Respiratory"
+        },
+        {
+            "id": 1151,
+            "clinical_rotation_type": "Pediatric Emergency Medicine"
+        },
+        {
+            "id": 1152,
+            "clinical_rotation_type": "Pediatric Healthy Lifestyles Medicine"
+        },
+        {
+            "id": 1153,
+            "clinical_rotation_type": "Pediatric Hematology / Oncology"
+        },
+        {
+            "id": 1154,
+            "clinical_rotation_type": "Pediatric Cardiothoracic Surgery"
+        },
+        {
+            "id": 1155,
+            "clinical_rotation_type": "Prevention and Health Promotion"
+        },
+        {
+            "id": 1156,
+            "clinical_rotation_type": "Primary Care"
+        },
+        {
+            "id": 1157,
+            "clinical_rotation_type": "Family Medicine"
+        },
+        {
+            "id": 1158,
+            "clinical_rotation_type": "Internal Medicine"
+        },
+        {
+            "id": 1159,
+            "clinical_rotation_type": "Geriatrics"
+        },
+        {
+            "id": 1160,
+            "clinical_rotation_type": "Pulmonary Medicine"
+        },
+        {
+            "id": 1161,
+            "clinical_rotation_type": "Radiology"
+        },
+        {
+            "id": 1162,
+            "clinical_rotation_type": "General Radiology"
+        },
+        {
+            "id": 1163,
+            "clinical_rotation_type": "Interventional Radiology"
+        },
+        {
+            "id": 1164,
+            "clinical_rotation_type": "Neuroradiology"
+        },
+        {
+            "id": 1165,
+            "clinical_rotation_type": "Rheumatology"
+        },
+        {
+            "id": 1166,
+            "clinical_rotation_type": "Surgery"
+        },
+        {
+            "id": 1167,
+            "clinical_rotation_type": "General Surgery"
+        },
+        {
+            "id": 1168,
+            "clinical_rotation_type": "Maxillofacial Surgery"
+        },
+        {
+            "id": 1169,
+            "clinical_rotation_type": "Micrographic Dermatologic Surgery (Mohs)"
+        },
+        {
+            "id": 1170,
+            "clinical_rotation_type": "Neurosurgery"
+        },
+        {
+            "id": 1171,
+            "clinical_rotation_type": "Otolaryngology"
+        },
+        {
+            "id": 1172,
+            "clinical_rotation_type": "Plastic and Reconstructive Surgery"
+        },
+        {
+            "id": 1173,
+            "clinical_rotation_type": "Pre-Op / Pre-Operative Screening Unit"
+        },
+        {
+            "id": 1174,
+            "clinical_rotation_type": "Transplant Surgery"
+        },
+        {
+            "id": 1175,
+            "clinical_rotation_type": "Trauma Surgery"
+        },
+        {
+            "id": 1176,
+            "clinical_rotation_type": "Urology"
+        },
+        {
+            "id": 1177,
+            "clinical_rotation_type": "General Urology"
+        },
+        {
+            "id": 1178,
+            "clinical_rotation_type": "Urogynecology"
+        },
+        {
+            "id": 1179,
+            "clinical_rotation_type": "Women’s Health"
+        },
+        {
+            "id": 1180,
+            "clinical_rotation_type": "General OB/Gyn"
+        },
+        {
+            "id": 1181,
+            "clinical_rotation_type": "Maternal/Fetal Medicine"
+        },
+        {
+            "id": 1183,
+            "clinical_rotation_type": "Reproductive Endocrinology & Infertility"
+        }
+    ],
+    "message": "",
+    "from": "api-server"
+}
+
+
+def unique(list_c):
+    unique_list = []
+    for x in list_c:
+
+        if x not in unique_list:
+            unique_list.append(x)
+    for x in unique_list:
+         print
+         var = x,
+
+list_c = ['Family Medicine','General Surgery','Emergency Medicine','Internal Medicine','Obstetrics','Gynecology','Pediatrics','Geriatrics','Psychology','Pediatric Plastic Surgery','Orthopedics','Ambulatory Medicine','Psychiatry','Ambulatory Medicine II','Family Medicine Elective',"Women’s Health",'Dermatology','Reproductive Endocrinology & Infertility','Behavioral Medicine','Primary Care','Surgery','Rheumatology','Psychiatric','Orthopedic Surgery','AIMM Orthopedics','ACMF Gastroenterology','Pediatric Medicine','Cardiology',' Cardiothoracic Surgery','Asthma and Allergy','Infectious Disease','Pain Management','OBGYN','Mental Health','RADIOLOGY','CT SURGERY','Sports Medicine','Pain Management & Regenerative Medicine','Neonatology','Thoracic Surgery','ER','Trauma Surgery','Oncology','Colorectal Surger','Bariatric Surgery','Dermatology and MOHS Surgery ','Urology','INTERVENTIONAL RADIOLOGY','Rheumatology','Psych','Long-term Care','INFECTIOUS DISEASE','Occupational Medicine/Urgent Care','Emergency Room','Neurology','ADOLESCENT MEDICINE','SICU','Vascular Surgery','ENT/Facial Plastics','Pediatric Emergency Medicine','Elective Cardiology','Elective Dermatology','Surgery/ICU','Evidence Based Practice','Neonatal Intensive Care Unit','Global Health Field Study','Pulmonary Critical Care','Cardiology','OBGYN','Psychiatry','Pediatrics','General Surgery','Primary Care','Emergency Medicine','Cardiac surgery ICU','Internal Medicine I','Family Medicine','Obstetrics','Gynecology','Internal Medicine II','Ears, Nose, & Throat','Behavioral Health','Occupational Health','Geriatric Medicine','Orthopedic Trauma','Hospitalist','Ob','Gyn','Geriatric','Primary Care Rotation','Occupational Medicine','INPATIENT PEDIATRICS','EVIDENCE-BASED MEDICINE','FastMed Urgent Care','Spine Surgery','Orthopaedics','Spine','Neurosurgery','NICU','Pediatric Cardiology','Neonatal ICU','Pediatric Emergency Medicine','ENT','Hope Pediatrics','Addiction Medicine','Psychiatric ER','Endocrinology','Plastic Surgery','Family Practice','Family Practice Preceptorship','MEDICAL INTENSIVE CARE UNIT','Urgent Care','Inpatient','Neurology','Anesthesiology','Wound Care','Hospitalist Medicine','Primary Care Clerkship','CriAcal Care Medicine','Underserved Family Medicine','Pulmonary','Endocrine','Pain Management','Trauma Bay','Plastic and Reconstructive Surgery','Surgical Oncology','General and Vascular Surgery','Nephrology','Neonatal Intensive Care Unit','Endocrinology','Long Term Care','Child Family Health International','Surgical ICU','Neonatology','Rehab Medicine','Trauma Team']
+
+print("the unique values from 1st list is")
+unique(list_c)
